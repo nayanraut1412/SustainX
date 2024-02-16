36440f37bc9cdd655b9cf6656519bbc2b70d862c
Aditya Dhage <115439063+Aditya1634@users.noreply.github.com>
2024-02-16 14:58:10 +0530

    Added Firebase Authentication

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 import 'package:flutter/material.dart';
 import 'package:sustain_x/pages/cost_calculation.dart';
 import 'package:sustain_x/pages/notification.dart';
@@ -10,8 +12,13 @@ import 'package:sustain_x/pages/schedule_pickup.dart';
 import 'package:sustain_x/pages/settings.dart';
 import 'package:sustain_x/pages/start.dart';
 import 'package:sustain_x/pages/success_page.dart';
+import 'package:sustain_x/pages/auth_page.dart';
 
-void main() {
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
@@ -23,9 +30,10 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'SustainX',
-      initialRoute: '/',
+      home: AuthPage(),
+      // initialRoute: '/login',
       routes: {
-        '/' : (context) => Start(),
+        // '/' : (context) => Start(),
         '/home' : (context) => Home(),
         '/login' : (context) => Login(),
         '/register' : (context) => Register(),
@@ -36,6 +44,7 @@ class MyApp extends StatelessWidget {
         '/notifications' : (context) => Notifications(),
         '/cost_calculation' : (context) => CostCalculation(),
         '/pickup_history' : (context) => PickupHistory(),
+        '/auth_page' : (context) => AuthPage(),
       },
     );
   }
